2881224ea473d6e8bc531d219e87b659ef69f61d
Claudio-Bernardinetti <claudiobernardinetti@gmail.com>
2023-10-20 17:43:20 +0100

    add header

--- Layout/head.php
+++ Layout/head.php
@@ -1,3 +1,11 @@
 
 
+<header>
+  <nav class="nav justify-content-center flex-column flex-md-row bg-success">
+    <div class=" h1">
+      <a class="nav-link active text-dark" href="index-Home.php" aria-current="page">Cani e Gatti</a>
+    </div>
+  </nav>
+</header>
+
 <?php include __DIR__ . '/partials/header.php' ?>
--- Layout/partials/header.php
+++ Layout/partials/header.php
@@ -1,8 +1,16 @@
 <header>
-  <nav class="nav justify-content-center flex-column flex-md-row">
-    <a class="nav-link active" href="index-Home.php" aria-current="page">Home</a>
-    <a class="nav-link active" href="index-Dogs.php">Cani</a>
-    <a class="nav-link" href="index-Cats.php">Gatti</a>
-    <a class="nav-link" href="index-Discounts.php">Sconti</a>
+  <nav class="nav justify-content-center flex-column flex-md-row bg-success">
+    <div class="p-3 fs-4">
+      <a class="nav-link active text-light" href="index-Home.php" aria-current="page">Home</a>
+    </div>
+    <div class="p-3 fs-4">
+      <a class="nav-link text-light" href="index-Dogs.php">Cani</a>
+    </div>
+    <div  class="p-3 fs-4">
+      <a class="nav-link text-light" href="index-Cats.php">Gatti</a>
+    </div>
+    <div  class="p-3 fs-4">
+      <a class="nav-link text-light" href="index-Discounts.php">Sconti</a>
+    </div>
   </nav>
 </header>
