1d9e32c4fe7ba34b5e517092853a08c473ab6613
Claudio-Bernardinetti <claudiobernardinetti@gmail.com>
2023-10-20 17:25:36 +0100

    add indexes

---
+++ Layout/partials/header.php
@@ -0,0 +1,8 @@
+<header>
+  <nav class="nav justify-content-center flex-column flex-md-row">
+    <a class="nav-link active" href="index-Home.php" aria-current="page">Home</a>
+    <a class="nav-link active" href="index-Dogs.php">Cani</a>
+    <a class="nav-link" href="index-Cats.php">Gatti</a>
+    <a class="nav-link" href="index-Discounts.php">Sconti</a>
+  </nav>
+</header>
